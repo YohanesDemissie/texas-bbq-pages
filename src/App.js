68b03c8d8a033f47f05d343c465692bb2fd6a45e
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-08-21 02:22:40 -0700

    replacing hash router with browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './Components/Home';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
