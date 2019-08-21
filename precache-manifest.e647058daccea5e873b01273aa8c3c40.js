d51b978c33f82774b69979800167423064f20bf5
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-08-20 19:15:14 -0700

    Updates

---
+++ precache-manifest.e647058daccea5e873b01273aa8c3c40.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "97b3805d7567bf00acb1",
+    "url": "/texas-bbq-pages/static/js/runtime~main.575379a9.js"
+  },
+  {
+    "revision": "6993cb4b51896c4d60a6",
+    "url": "/texas-bbq-pages/static/js/main.35fe95a2.chunk.js"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/texas-bbq-pages/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "febb067a0dcda178e690d5bcd8163152",
+    "url": "/texas-bbq-pages/index.html"
+  }
+];
